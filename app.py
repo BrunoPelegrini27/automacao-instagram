b30933d7a0ae54e35d1ae2d685cdad42e5ddaf14
Bruno Pelegrini 42 <37117282+BrunoPelegrini27@users.noreply.github.com>
2025-02-14 10:41:29 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
-from flask import Flask, request, jsonify
-import requests
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.keys import Keys
-import time
+
+# Configuração do navegador em modo headless
+chrome_options = Options()
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument("--disable-dev-shm-usage")
+
+# Usar o ChromeDriver gerenciado pelo webdriver-manager
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
 
 app = Flask(__name__)
 
